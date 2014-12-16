ecb4c62d3cf9fabd301a3a9e5daca9b8f3210e94
andela-amaborukoje <adebayo@andela.co>
2014-12-16 13:46:42 +0100

    refactored using AngularJs

---
+++ js/main.js
@@ -0,0 +1,93 @@
+var gitHubApp = angular.module('gitHubApp', []);
+
+gitHubApp.controller('gitHubController', ['$scope', 'gitHubService', function($scope, gitHubService){
+      $scope.results= [];
+
+      $scope.displayInfo = function() {
+        gitHubService.getUrl($scope.search)
+        .success( function(data, status){
+          console.log(data.total_count +" and "+ status);
+            if(data.total_count >=1){
+              $scope.statusDisplay = "Displaying Result";
+              $scope.results= [];
+              angular.forEach( data.items, function( users, index ){
+                console.log($scope.results);
+                $scope.search="";
+                $scope.results.push( users );
+            });
+          }else {
+            $scope.results= [];
+             $scope.statusDisplay = "ERROR NAME NOT FOUND";
+            
+          };
+        })
+        .error( function( data, status ) {
+           $scope.statusDisplay = "Cannot Be processed at the Moment. Check your Internet Connection ERROR:"+ status;
+          $scope.results= [];
+        });
+      }
+
+}]);
+
+gitHubApp.factory('gitHubService', function($http){
+      
+    var url = "https://api.github.com/search/users?";
+    var Info = {};
+
+    Info.getUrl = function( query ){
+      return $http.get( url, { params: { q: query, order:"desc", sort:"joined" }});
+   };
+   
+    return Info;
+});
+
+
+
+//USING THE .SERVICE
+
+/* 
+var gitHubApp = angular.module('gitHubApp', []);
+
+gitHubApp.controller('gitHubController', ['$scope', 'gitHubService', function($scope, gitHubService){
+      $scope.results= [];
+
+      $scope.displayInfo = function() {
+        gitHubService.getUrl($scope.search)
+        .success( function(data, status){
+          console.log(data.total_count +" and "+ status);
+            if(data.total_count >=1){
+              $scope.statusDisplay = "Displaying Result";
+              $scope.results= [];
+              angular.forEach( data.items, function( users, index ){
+                console.log($scope.results);
+                $scope.search="";
+                $scope.results.push( users );
+            });
+          }else {
+            $scope.results= [];
+             $scope.statusDisplay = "ERROR NAME NOT FOUND";
+            
+          };
+        })
+        .error( function( data, status ) {
+           $scope.statusDisplay = "Cannot Be processed at the Moment. Check your Internet Connection ERROR:"+ status;
+          $scope.results= [];
+        });
+      }
+
+}]);
+
+gitHubApp.service('gitHubService', function($http){
+      
+    var url = "https://api.github.com/search/users?";
+  //  var Info = {};
+
+    this.getUrl = function( query ){
+      return $http.get( url, { params: { q: query, order:"desc", sort:"joined" }});
+   };
+   
+ //   return ;
+});
+
+
+*/
